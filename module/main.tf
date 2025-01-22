7f504dc3f7c370c08bf62870c98f4fd902d1d326
s poorna chandra <poornanaidu1@gmail.com>
2025-01-22 11:14:04 +0530

    ec2 creation

--- module/main.tf
+++ module/main.tf
@@ -17,19 +17,3 @@ resource "aws_route53_record" "record" {
   ttl     = 30
   records = [aws_instance.instance.private_ip]
 }
-
-resource "null_resource" "ansible" {
-
-  depends_on = [
-    aws_route53_record.record
-  ]
-
-  provisioner "local-exec" {
-    command = <<EOF
-cd /home/centos/roboshop-ansible
-git pull
-sleep 30
-ansible-playbook -i ${var.name}-dev.devopsproject.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
-EOF
-  }
-}
