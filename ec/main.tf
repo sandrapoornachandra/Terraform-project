e4cad0c3330682b74688a00c0e4737c010c8611f
s poorna chandra <poornanaidu1@gmail.com>
2025-01-22 11:07:28 +0530

    ec2 creation

--- ec/main.tf
+++ ec/main.tf
@@ -5,8 +5,8 @@ module "components" {
   source          = "./module"
   zone_id         = var.zone_id
   security_groups = var.security_groups
-#   name            = each.value["name"]
-#   instance_type   = each.value["instance_type"]
+  name            = each.value["name"]
+  instance_type   = each.value["instance_type"]
 }
 
 
